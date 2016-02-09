c93e8c6aec6b62c58dae0e38a37d2377913430bd
felixfir <felix.ibarra.rdz@outlook.com>
2016-02-09 00:50:17 -0600

    Agregué la hora en crear partidas.
    Agregué la hora en crear partidas.

--- app/src/main/java/com/jennifer/controller/CrearPrivadasService.java
+++ app/src/main/java/com/jennifer/controller/CrearPrivadasService.java
@@ -20,24 +20,22 @@ public class CrearPrivadasService extends AsyncTask<String, String, String> {
     String titulo;
     String descripcion;
     String fecha;
-    String tipoApuesta;
+    String hora;
+    int tipoApuesta;
     Double premio;
-    int contadorUsuarios;
-    int contadorRespuestas;
     List<String> usuarios = new ArrayList<String>();
     List<String> respuestas = new ArrayList<String>();
 
-    public CrearPrivadasService(Activity activity, String titulo, String descripcion, String fecha, String tipoApuesta, Double premio, List<String> usuarios, List<String> respuestas, int contadorUsuarios, int contadorRespuestas) {
+    public CrearPrivadasService(Activity activity, String titulo, String descripcion, String fecha, String hora, int tipoApuesta2, Double premio, List<String> usuarios, List<String> respuestas) {
         this.activity = activity;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.fecha = fecha;
-        this.tipoApuesta = tipoApuesta;
+        this.hora = hora;
+        this.tipoApuesta = tipoApuesta2;
         this.premio = premio;
         this.usuarios = usuarios;
         this.respuestas = respuestas;
-        this.contadorUsuarios = contadorUsuarios;
-        this.contadorRespuestas = contadorRespuestas;
     }
 
     @Override
@@ -64,12 +62,14 @@ public class CrearPrivadasService extends AsyncTask<String, String, String> {
 
         parametros.append("name").append("=").append(titulo).append("&")
                 .append("description").append("=").append(descripcion).append("&")
-                .append("dateTime").append("=").append(fecha).append("&")
+                .append("date").append("=").append(fecha).append("&")
+                .append("time").append("=").append(hora).append("&")
                 .append("typeBet").append("=").append(tipoApuesta).append("&")
                 .append("award").append("=").append(premio).append("&")
                 .append("users").append("=").append(users).append("&")
                 .append("options").append("=").append(options);
 
+        System.out.println(fecha);
         System.out.println("Users: " + users);
         System.out.println("Options: " + options);
         System.out.println("Parametros: " + parametros.toString());
